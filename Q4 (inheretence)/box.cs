abe657a7cb79c37402ece16a95210d9bc6f5cddf
yusef159 <yusefaborokon@gmail.com>
2021-04-28 18:05:00 +0300

    added eqauls

--- Q4 (inheretence)/box.cs
+++ Q4 (inheretence)/box.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Q4__inheretence_
 {
-    class box
+    public abstract class box
     {
         //תכונות של מחלקה
         private decimal length;
@@ -24,18 +24,48 @@ namespace Q4__inheretence_
             this.color = color;
         }
 
-        public float area_calculate()//שיטה לחישוב שטח של קופסה
+        public virtual float area_calculate()//שיטה לחישוב שטח של קופסה
         {
             return (float)(2 * (width * length) + 2 * (width * hight) + 2 * (length * hight));
         }
 
-        public float capacity_calculate()//שיטה לחישוב נפח של קופסה
+        public virtual float capacity_calculate()//שיטה לחישוב נפח של קופסה
         {
             return (float)(length * width * hight);
         }
      
-
-
-        
     }
+
+
+    public class Equals:box
+    {
+        //תכונות של מחלקה
+        private decimal length;
+        private decimal hight;
+        private decimal width;
+        private string color;
+
+
+
+        //בנאי מקבל ערכים 
+        public Equals(decimal length, decimal hight, decimal width, string color)
+        {
+            this.length = length;
+            this.hight = hight;
+            this.width = width;
+            this.color = color;
+        }
+
+        public override float area_calculate()//שיטה לחישוב שטח של קופסה
+        {
+            return (float)(2 * (width * length) + 2 * (width * hight) + 2 * (length * hight));
+        }
+
+        public override float capacity_calculate()//שיטה לחישוב נפח של קופסה
+        {
+            return (float)(length * width * hight);
+        }
+    }
+
+
 }
